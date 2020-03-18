d694ce38b5b38ad63e9e6c6b532e861d19b769b3
kfly <chuangshiding@gmail.com>
2020-03-18 15:34:46 +0800

    spark sql source

--- travel_common/src/main/java/com/travel/common/Constants.java
+++ travel_common/src/main/java/com/travel/common/Constants.java
@@ -66,4 +66,27 @@ public class Constants {
     public final static Integer DEFAULT_REGION_NUM = 8;
 
 
+    /**
+     * add config
+     * @author dingchuangshi
+     */
+    // stat
+    public final static String HBASE_TABlE_NAME = "hbase_table_name";
+
+    public final static String SPARK_SQL_TABlE_SCHEMA = "sparksql_table_schema";
+
+    public final static String HBASE_TABLE_SCHEMA = "hbase_table_schema";
+
+
+    public final static String HBASE_ZOOKEEPER_QUORUM = "hbase.zookeeper.quorum";
+
+    public final static String HBASE_ZOOKEEPER_PROPERTY_CLIENT_PORT = "hbase.zookeeper.property.clientport";
+
+    public final static String HBASE_CLIENT_OPERATION_TIMEOUT = "hbase.client.operation.timeout";
+
+    public final static String ZOOKEEPER_ZNODE_PARENT = "zookeeper.znode.parent";
+
+    // end
+
+
 }
